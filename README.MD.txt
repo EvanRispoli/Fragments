120285ea9c69faa18f4e1e91858ca0b30581591d
Evan Rispoli <evan.rispoli@gmail.com>
2022-08-23 22:09:20 -0400

    Create a new fragment

---
+++ README.MD.txt
@@ -0,0 +1,4 @@
+1 - View binding on graddle
+2 - private  lateinit var binding: ActivityMainBinding no main activity
+3 - Remove setContentView(R.layout.activity_main)
+4 - Create a new fragment(blank)
--- app/src/main/java/com/example/fragments/MainActivity.kt
+++ app/src/main/java/com/example/fragments/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.fragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.fragments.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+//0002
+    private  lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        0003
+//        setContentView(R.layout.activity_main)
+//        0004
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
