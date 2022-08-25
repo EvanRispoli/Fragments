48e3f9209a9d72215fae243c828ee1b71f7164d8
Evan Rispoli <evan.rispoli@gmail.com>
2022-08-24 20:06:19 -0400

    Use view binding in fragments

--- app/src/main/java/com/example/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/fragments/HomeFragment.kt
@@ -5,19 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+import com.example.fragments.databinding.FragmentHomeBinding
+
 class HomeFragment : Fragment() {
+    /** Binding faz a vinculacao do layout com a classe **/
+    private var _binding: FragmentHomeBinding? = null
+
+    private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        //return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
 
 }
